b16d088abc0547cb409b1d99204a6b339a7a464d
Jorge <jorgebreadgames@gmail.com>
2018-09-19 16:44:56 -0500

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,15 @@
 //Puerto
 
 process.env.PORT =process.env.PORT ||3000;
+//Entorno
+process.env.NODE_ENV=process.env.NODE_ENV || 'dev';
+//BASE DE DATOD
+
+let urlDB;
+if(process.env.NODE_ENV==='dev'){
+    urlDB='mongodb://localhost:27017/cafebar'
+}else{
+
+    urlDB='mongodb://cafeuser:Jorge75432560@ds163382.mlab.com:63382/cafebar'
+}
+process.env.URLDB=urlDB;
