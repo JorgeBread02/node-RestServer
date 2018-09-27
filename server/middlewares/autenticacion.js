58014776ec85f716339c49708361a553232079c3
Jorge <jorgebreadgames@gmail.com>
2018-09-27 10:35:55 -0500

    Subida de archivos

--- server/middlewares/autenticacion.js
+++ server/middlewares/autenticacion.js
@@ -42,8 +42,31 @@ next();
 
 
 }
+//=========
+//Verificar token para imagen
+//==========
+let VerificaTokenImg=(req,res,next)=>{
+let token = req.query.token;
 
+jwt.verify(token,process.env.SEED,(err,decoded)=>{
+
+
+    if(err){
+        return res.status(401).json({
+            ok:false,
+            err:{
+                message:'token no valido amigaso'
+            }
+        });
+    }
+    req.usuario=decoded.usuario;
+    next();
+});
+
+
+}
 module.exports={
     VerificaToken,
+    VerificaTokenImg,
     verificaAdmin_Role
 }
